0ab0d8798a6b6b5a778a4bebc294682a8d9fc4e1
nachoscocco <nachoscocco14@gmail.com>
2020-05-22 17:40:32 -0300

    rutas completas

---
+++ src/app/app.routes.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { AboutComponent } from './components/about/about.component';
+
+const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'heroes', component: HeroesComponent },
+    { path: 'about', component: AboutComponent },
+    { path: '**', redirectTo: 'home' }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
